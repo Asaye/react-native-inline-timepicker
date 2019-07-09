dda8b3e0a44791614715939415bdcd92cf4a58ca
Asaye Dilbo <32922135+Asaye@users.noreply.github.com>
2019-07-09 11:21:34 +0300

    Add files via upload

---
+++ styles/Colors.js
@@ -0,0 +1,36 @@
+const Colors_default = {
+	"timepicker_txt": "#ddd",
+	"timepicker_back": "#777",
+	"timepicker_cont": "#555",	
+	"timepicker_active": "#000",
+	"timepicker_border": "#555",
+};
+
+const Colors_greenish = {
+	"timepicker_txt": "#eee",
+	"timepicker_back": "#4caf50",
+	"timepicker_cont": "#66bb6a",
+	"timepicker_active": "#1b5e20",
+	"timepicker_border": "#007E33",
+};
+
+const Colors_bluish = {
+	"timepicker_txt": "#eee",
+	"timepicker_back": "#2196f3", 
+	"timepicker_cont": "#29b6f6",
+	"timepicker_active": "#1a237e",
+	"timepicker_border": "#1565c0",
+};
+
+const Colors_reddish = {
+	"timepicker_txt": "#eee",
+	"timepicker_back": "#ef5350", 
+	"timepicker_cont": "#ff7043",
+	"timepicker_active": "#b71c1c",
+	"timepicker_border": "#c62828",
+};
+
+module.exports.Colors_default = Colors_default;
+module.exports.Colors_greenish = Colors_greenish;
+module.exports.Colors_bluish = Colors_bluish;
+module.exports.Colors_reddish = Colors_reddish;
